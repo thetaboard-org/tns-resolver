f473f3843c48bcdf2afff90f3a7c9a8820d1383c
Adrien Vinches <ad.vinches@gmail.com>
2022-03-02 18:09:48 -0800

    add custom RPC endpoint

--- README.md
+++ README.md
@@ -1,53 +1,68 @@
-# Theta Name Service
+# Theta Name Service - TNS Resolver
+
+###### This NPM can be used to retrieve an address from a TNS or a TNS from an address on the Theta Blockchain.
+###### It can also retrieve a list of domain names from a list of addresses.
+
 ### Installation
 To install tns-resolver use the following statement:
 ```console
 npm install tns-resolver
 ```
 
-### Import
-To import tns-resolver use the following statement:
+### Import and instanciate TNS
+To import and instanciate tns-resolver use the following statement:
 ```js script
-import { getDomainName, getDomainNames, getAddress } from 'tns-resolver';
+import TNS from "tns-resolver";
+
+const tns = new TNS();
 ```
 
 ### Get domain name from an address
 To get the domain name from an address use the ```getDomainName``` function such as: 
 ```js script
-import { getDomainName } from 'tns-resolver';
+import TNS from 'tns-resolver';
 
 const address = '0x123...';
-const domainName = await getDomainName(address);
+const tns = new TNS();
+const domainName = await tns.getDomainName(address);
 console.log(domainName);
 /* will return the domain name 
- or null if no domain name is assigned on this address
+ or null if no domain name is assigned to this address
  i.e. will log 'domainname.theta' or null */
+```
+
+### Get address from a domain name
+To get the address from a domain name use the ```getAddress``` function such as:
+```js script
+import TNS from 'tns-resolver';
+
+const domainName = 'test.theta';
+const tns = new TNS();
+const address = await tns.getAddress(domainName);
+console.log(address);
+/* will return the address or null if the domain
+is not assigned to an address
+i.e. will log '0x123...' or null */
 ```
 
 ### Get domain names for a list of addresses
 If you need to process many addresses (eg: showing domain names of transaction histories), use the ```getDomainNames``` function such as: 
 ```js script
-import { getDomainNames } from 'tns-resolver';
+import TNS from 'tns-resolver';
 
 const addresses = ['0x123...', '0x456...'];
-const domainNames = await getDomainNames(addresses);
+const tns = new TNS();
+const domainNames = await tns.getDomainNames(addresses);
 console.log(domainNames);
-/* will return an object where 
- the key is the address 
- the value is the domain name or 
- an empty string if no domain name is assigned on this address
+/* will return an object where the key is the address
+ and the value is the domain name or an empty string if no domain name is assigned on this address
  i.e. will log { "0x123...": "domainname.theta", "0x456...": "" } */
 ```
 
-### Get address from a domain name
-To get the address from a domain name use the ```getAddress``` function such as: 
+### Instanciate TNS with a custom RPC endpoint
+To instanciate tns-resolver using a custom RPC endpoint use the following statement:
 ```js script
-import { getAddress } from 'tns-resolver';
+import TNS from "tns-resolver";
 
-const domainName = 'test.theta';
-const address = await getAddress(domainName);
-console.log(address);
-/* will return the address or null if the domain 
-is not assigned to an address
-i.e. will log '0x123...' or null */
+const tns = new TNS({customRpcEndpoint: 'YOUR_CUSTOM_RPC_ENDPOINT_HERE'});
 ```
--- index.js
+++ index.js
@@ -1,81 +1,26 @@
-import { ethers } from "ethers";
-import { namehash, isAddress} from "./utils/utils";
+import { ethers } from "ethers"
+import { namehash, isAddress} from "./utils/utils"
 import { toChecksumAddress } from "ethereum-checksum-address"
-const ethNamehash = require('eth-ens-namehash');
 
+const ethNamehash = require('eth-ens-namehash')
 const resolverABI = require("./contracts/PublicResolver.json")
 const reverseRecordsABI = require("./contracts/ReverseRecords.json")
-const EMPTY_ADDRESS = '0x0000000000000000000000000000000000000000';
-const provider = new ethers.providers.JsonRpcProvider("https://eth-rpc-api.thetatoken.org/rpc");
-const resolverContract = new ethers.Contract("0x9f0a9D6788FA98E50Ed1cA062abd1F69BC6C3A12", resolverABI, provider)
-const reverseRecordsContract = new ethers.Contract("0xE6E9371993126e67B38041c2eE032480009AAd8C", reverseRecordsABI, provider)
+const EMPTY_ADDRESS = '0x0000000000000000000000000000000000000000'
+const defaultRpc = 'https://eth-rpc-api.thetatoken.org/rpc'
 
 
-//Get reverse names for a list of addresses.
-export const getDomainNames = async(addresses, extension) => {
-    try {
-        let addressesToReverse = []
-        let result = {}
-        for (const address of addresses) {
-            if (isAddress(address)) {
-                const checksummedAddress = toChecksumAddress(address)
-                addressesToReverse.push(checksummedAddress)
-            } else {
-                addressesToReverse.push(address)
-            }
-        }
-        const reversedNames = await getReverseNames(addressesToReverse, extension)
-        const validReversedNames = reversedNames.map((n) => { return ethNamehash.normalize(n) === n ? n : '' })
-        addresses.forEach((key, i) => result[key] = validReversedNames[i])
-        return result
-    } catch (e) {
-        console.log(`Error in getDomainNames`, e)
-        return null
-    }
+const getResolvercontract = (provider) => {
+    return new ethers.Contract("0x9f0a9D6788FA98E50Ed1cA062abd1F69BC6C3A12", resolverABI, provider)
 }
 
-//Get reverse name of address. Returns null if user has not set it.
-export const getDomainName = async(address) => {
-    try {
-        if (isAddress(address)) {
-            const checksummedAddress = toChecksumAddress(address)
-            const domain = await getReverseName(checksummedAddress)
-            if (domain) {
-                const addressRecord = await getAddressRecord(domain)
-                if (addressRecord && addressRecord === checksummedAddress) {
-                    return domain + ".theta"
-                }
-            }    
-        }
-        return null
-    } catch (e) {
-        console.log(`Error in getDomainName`, e)
-        return null
-    }
+const getReverseRecordsContract = (provider) => {
+    return new ethers.Contract("0xE6E9371993126e67B38041c2eE032480009AAd8C", reverseRecordsABI, provider)
 }
 
-
-//Returns record address of domain if it matches the reversename
-export const getAddress = async(domain) => {
-    try {
-        const formattedDomain = domain.toLowerCase().replace('.theta', '')
-        const addressRecord = await getAddressRecord(formattedDomain)
-        if (addressRecord && addressRecord !== EMPTY_ADDRESS) {
-            const reverseName = await getReverseName(addressRecord)
-            if (reverseName && reverseName === formattedDomain) {
-                return addressRecord
-            }
-        }
-        return null    
-    } catch (e) {
-        console.log(`Error getAddress`, e)
-        return null
-    }
-}
-
-const getAddressRecord = async(domain) => {
+const getAddressRecord = async(domain, provider) => {
     try {
         const label = namehash(domain + '.theta')
+        const resolverContract = getResolvercontract(provider)
         return await resolverContract['addr(bytes32)'](label)   
     } catch (e) {
         console.log(`Error getAddressRecord for resolverContract`, e)
@@ -83,10 +28,11 @@ const getAddressRecord = async(domain) => {
     }
 }
 
-const getReverseName = async(address) => {
+const getReverseName = async(address, provider) => {
     try {
         const reverseNode = `${address.slice(2)}.addr.reverse`
         const reverseNamehash = namehash(reverseNode)
+        const resolverContract = getResolvercontract(provider)
         return await resolverContract.name(reverseNamehash)
     } catch (e) {
         console.log(`Error getReverseName for resolverContract`, e)
@@ -94,12 +40,96 @@ const getReverseName = async(address) => {
     }
 }
 
-const getReverseNames = async(addresses, extension) => {
+const getReverseNames = async(addresses, extension, provider) => {
     try {
         if (!extension) extension = 'theta'
+        const reverseRecordsContract = getReverseRecordsContract(provider)
         return await reverseRecordsContract.getNames(addresses, extension)
     } catch (e) {
         console.log(`Error getReverseNames for reverseRecordsContract`, e)
         return null
+    }
+}
+
+export default class TNS {
+    constructor(options) {
+        const { customRpcEndpoint } = options || {};
+        let rpcEndpoint = customRpcEndpoint ? customRpcEndpoint : defaultRpc
+        this.provider = new ethers.providers.JsonRpcProvider(rpcEndpoint)
+    }
+
+    //Returns record address of domain if it matches the reversename
+    async getAddress(domain) {
+        try {
+            const formattedDomain = domain.toLowerCase().replace('.theta', '')
+            const addressRecord = await getAddressRecord(formattedDomain, this.provider)
+            if (addressRecord && addressRecord !== EMPTY_ADDRESS) {
+                const reverseName = await getReverseName(addressRecord, this.provider)
+                if (reverseName && reverseName === formattedDomain) {
+                    return addressRecord
+                }
+            }
+            return null
+        } catch (e) {
+            console.log(`Error getAddress`, e)
+            return null
+        }
+    }
+
+
+    async getDomainNames(addresses, extension) {
+        try {
+            let addressesToReverse = [],
+             result = {},
+             count = 0,
+             reversedNames = []
+            for (const address of addresses) {
+                if (isAddress(address)) {
+                    const checksummedAddress = toChecksumAddress(address)
+                    addressesToReverse.push(checksummedAddress)
+                } else {
+                    addressesToReverse.push(address)
+                }
+                if (count <= 2000) {
+                    count++
+                } else {
+                    const tempreversedNames = await getReverseNames(addressesToReverse, extension, this.provider)
+                    reversedNames.push(...tempreversedNames)
+                    addressesToReverse = []
+                    count = 0
+                }
+            }
+            if (addressesToReverse.length) {
+                const tempreversedNames = await getReverseNames(addressesToReverse, extension, this.provider)
+                reversedNames.push(...tempreversedNames)
+                addressesToReverse = []
+            }
+            const validReversedNames = reversedNames.map((n) => { return ethNamehash.normalize(n) === n ? n : '' })
+            addresses.forEach((key, i) => result[key] = validReversedNames[i])
+            return result
+        } catch (e) {
+            console.log(`Error in getDomainNames`, e)
+            return null
+        }
+    }
+
+    //Get reverse name of address. Returns null if user has not set it.
+    async getDomainName(address) {
+        try {
+            if (isAddress(address)) {
+                const checksummedAddress = toChecksumAddress(address)
+                const domain = await getReverseName(checksummedAddress, this.provider)
+                if (domain) {
+                    const addressRecord = await getAddressRecord(domain, this.provider)
+                    if (addressRecord && addressRecord === checksummedAddress) {
+                        return domain + ".theta"
+                    }
+                }
+            }
+            return null
+        } catch (e) {
+            console.log(`Error in getDomainName`, e)
+            return null
+        }
     }
 }
